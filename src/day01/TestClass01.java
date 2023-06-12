8d140eff9d041996273b2f832ce5c0179f6bb410
jaeyeon <chlwodus1202@naver.com>
2023-06-12 14:27:30 +0900

    day01 프로젝트

---
+++ src/day01/TestClass01.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class TestClass01 {
+	public static void main(String[] args) {
+		
+		//괄호 안의 내용을 출력해주는 기능
+		System.out.println(100);
+		System.out.println("안녕하세요");
+		
+		//:주석 -> 실행하지 않는다, 부연설명
+		//System.out.println("주석");
+		
+		System.out.print("1. 안녕");
+		System.out.print("2. 안녕");
+		System.out.print("3. 안녕");
+		
+	}
+
+}
